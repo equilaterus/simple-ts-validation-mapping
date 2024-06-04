afae121e284522b141b9d4cd8361dfa3729a98ab
dacanizares <dacanizares@outlook.com>
2024-06-03 22:40:02 -0500

    Added initial code

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # simple-ts-validator
+
+## Usage
+
+Define your commands:
+
+```ts
+export class CreateUserCommand {
+  requiredField: string = "";
+  optionalField: string | null = null;
+}
+```
+
+And validate, for example on ExpressJS:
+
+```ts
+const [command, isValid, validationErrors] = validateModel<CreateUserCommand>(CreateUserCommand, req.body);
+    
+if (!isValid) {
+    res.status(400).send(validationErrors);
+} else {
+    const result = await createEmployee({
+        additionalFields: true,
+        ...command,
+    } as NewUser)
+
+    res.send(result);
+}
+```
---
+++ validator.ts
@@ -0,0 +1,41 @@
+function validateModel<T extends object>(type: { new(): T ;}, req: {[key: string]: any}) 
+  : [T, boolean, Array<string>] {
+  const command = new type();
+  type keys = keyof T;
+
+  let isValid = true;
+  const validationErrors: Array<string> = [];
+  const requiredKeys: Array<string> = [];
+  const addedKeys: Array<string> = [];
+
+  Object.entries(command).forEach(([key, value]) => {
+    if (value !== null) {
+      requiredKeys.push(key);
+    }
+  });
+  
+  Object.entries(req).forEach(([key,value]) => {
+    if (key in command) {
+      command[key as keys] = value as any;
+      addedKeys.push(key);
+    } else {
+      isValid = false;
+      validationErrors.push(`Body key ${key} is invalid`);
+    }
+  });
+
+  requiredKeys.forEach((key) => {
+    if (!addedKeys.includes(key)) {
+      validationErrors.push(`Body key ${key} is missing`);
+      isValid = false;
+    }
+  });
+
+  return [
+    command,
+    isValid,
+    validationErrors
+  ]
+}
+
+module.exports = validateModel;
